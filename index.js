e78da3328624901e8539047ea15bb4cb15ea7b6b
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-19 00:51:36 +0300

    time not working

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Your code here
+
+//create employee records
+function createEmployeeRecord(employee){
+    return{
+        firstName:employee[0],
+        familyName:employee[1],
+        title:employee[2],
+        payPerHour:employee[3],
+        timeInEvents:[],
+        timeOutEvents:[],
+    }
+}
+
+function createEmployeeRecords(employeeData){
+ return employeeData.map(employee=>{
+    return createEmployeeRecord(employee)
+ })
+}
+
+function createTimeInEvent(employee,dates){
+    
+}
+
+function createTimeOutEvent(employee,dates){
+   
+}
